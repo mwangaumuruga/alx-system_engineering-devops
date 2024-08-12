bafa3d7c385516f106004d8d12ee7f3646720e29
mwangaumuruga <119576330+mwangaumuruga@users.noreply.github.com>
2024-08-12 09:19:16 +0000

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-#sing Puppet, create a manifest that kills a process named killmenow.
+#using Puppet, create a manifest that kills a process named killmenow.
 
-exec {'pkil -f 'killmenow':
+exec { 'pkill -f killmenow':
   path  => '/usr/bin/:/usr/local/bin/:/bin/',
 }
